c2e395fae36e50b1d0024424beb16565575aa600
Shameel Latif <Shameel@Shameels-MacBook-Pro.local>
2017-11-11 20:19:14 -0500

    commit

---
+++ test4.py
@@ -0,0 +1,22 @@
+html_str = """<html>
+	<body>
+		<div id="main" class="section">
+			<span><h2>Hello, 206!</h2></span>
+			<a href="http://piazza.com">Piazza</a>
+		</div>
+		<div id="secondary" class="section">
+			<ol>
+				<li><a href="http://umich.edu">UM</a></li>
+				<li>This one is just text.</li>
+			</ol>
+		</div>
+	</body>
+</html>"""
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(html_str,'html.parser')
+lst = soup.find_all('a')
+for element in lst:
+	print((element['href']).text)
+	
